a7bfcd080b7fc3d937cc1494eb4245a381b5ea5f
Noah <noahm0217@gmail.com>
2021-10-06 13:06:17 -0600

    file structure readt & server js complete

---
+++ server.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+const express = require('express');
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+//server connection
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(require('./routes'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/social-network-api', {
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+    useNewUrlParser: true
+});
+
+mongoose.set('debug', true);
+
+//server successfully connected\
+app.listen(PORT, () => console.log(`Successfully connected to:${PORT}`));
