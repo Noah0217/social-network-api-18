b2b2750852a0ce0e8c9d065866ba866e3b52f42c
Noah <noahm0217@gmail.com>
2021-10-08 16:55:06 -0600

    index for api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,1 +1,8 @@
-//
+const userRoutes = ('./user-routes');
+const router = require('express').Router();
+const thoughRoutes = require('./though-routes');
+
+router.use('thoughts', thoughtRoutes);
+router.use('/users', userRoutes);
+
+module.exports = router;
