1f57b255e49bf785cd1690676df6e5fb28dc1dde
Noah <noahm0217@gmail.com>
2021-10-08 16:50:21 -0600

    index for routes

--- routes/index.js
+++ routes/index.js
@@ -1,1 +1,13 @@
-//
+const apiRoutes = require('./api');
+const router = require('express').Router();
+
+//api routes
+router.use("api", apiRoutes);
+
+//error message
+router.use((req, res) => {
+    res.status(404).send
+}
+);
+
+module.exports - router;
