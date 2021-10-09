2325767c6a7a189aebd40d9c1a2777b18405bb69
Noah <noahm0217@gmail.com>
2021-10-09 15:29:02 -0600

    Users models complete

--- models/User.js
+++ models/User.js
@@ -1,1 +1,46 @@
-//
+const { Schema, model } = require('mongoose');
+
+//Users Schema
+const UsersSchema = new Schema(
+    
+{
+    username: {
+      type: String,
+      Unique: true,
+      required: true,
+      trim: true
+    },
+    email: {
+      type: String,
+      required: true,
+      Unique: true,
+      trim: true
+    },
+    thoughts: {
+      type: Schema.Types.ObjectId,
+      ref: 'Thought'
+    },
+    friends: {
+      type: Schema.Types.ObjectId,
+      ref: 'User'
+    },
+
+  },
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true
+    },
+    id: false
+}
+
+);
+
+//counts friends
+const User = model("User", userSchema);
+
+userSchema.virtual("friendCount").get(function () {
+    return this.friends.length;
+  });
+
+  module.exports = User;
