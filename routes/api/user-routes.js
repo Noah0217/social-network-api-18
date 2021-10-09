98d535623056eec0d05ce585f37ddb783dbb83cf
Noah <noahm0217@gmail.com>
2021-10-09 00:20:42 -0600

    user-routes complete

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,1 +1,30 @@
-//
+const router = require("express").Router();
+
+const {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  removeFriend,
+  //routes user/controller
+} = require("../../controllers/user-controller");
+
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
+
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+router
+    .route('/:userId/friends/:friendId')
+    .post(addFriend)
+    .delete(removeFriend)
+
+module.exports = router;
