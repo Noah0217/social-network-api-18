bbaca51436a4becaeec43b35c561ce0e34010d5f
Noah <noahm0217@gmail.com>
2021-10-09 00:09:24 -0600

    routes thought-routes complete

--- routes/api/thought-routes.js
+++ routes/api/thought-routes.js
@@ -1,1 +1,29 @@
-//
+const router = require('express').Router();
+
+const {
+    getAllThoughts,
+    createThought,
+    getThoughtById,
+    updateThought,
+    removeReaction,
+    addReaction,
+    deleteThought
+    //route to thought controller
+} = require('../../controllers/thought-controller')
+
+router
+    .route('/:id')
+    .get(getThoughtById)
+    .put(updateThought)
+    .delete(deleteThought);
+
+    router
+    .route('/')
+    .get(getAllThoughts)
+    .post(createThought);
+
+    router.route('/:thoughtId/reactions/')
+    .delete(deleteReaction)
+    .post(addReaction)
+
+module.exports = router;
