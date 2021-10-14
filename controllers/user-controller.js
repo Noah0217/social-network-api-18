37337bdd4e62a232afacdb845c7b55d9901b13b8
Noah <noahm0217@gmail.com>
2021-10-13 18:51:14 -0600

    fixed 400 bad request error

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -38,10 +38,10 @@ const userController = {
 
 
  createUser({ body }, res) {
-    User.create(body)
-.then(dbUserData => res.json(dbUserData))
-    .catch(err => res.status(400).json(err));
-},
+     User.create(body)
+ .then(dbUserData => res.json(dbUserData))
+     .catch(err => res.json(err));
+ },
 
 
 
